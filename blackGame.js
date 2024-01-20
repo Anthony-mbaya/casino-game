1b9b4128e8ce8a9a6343e92d0444d988604c9292
Anthony-Mbaya <120335962+Anthony-mbaya@users.noreply.github.com>
2024-01-20 11:08:45 +0300

    modified

--- blackGame.js
+++ blackGame.js
@@ -4,7 +4,8 @@ let newCardButton = document.getElementById("newCard");
 let newGameButton = document.getElementById("newGame");
 let sumValEl = document.getElementById("sumVal");
 let newCardMsgEl = document.getElementById("newCardMsg");
-
+let gameOn = false;
+let live = false;
 const getRandomCard = () =>{
     let randomCard = Math.floor(Math.random() * 13) + 1; 
     randomCard === 1 ? randomCard = 11 :randomCard > 10 && randomCard <= 13 ? randomCard = 10 : randomCard;
@@ -17,34 +18,39 @@ let sCard = getRandomCard();
 const cardsArray = [fCard,sCard];
 let sum = 0;
 sum = cardsArray[0] + cardsArray[1];
-
-
+ 
 const startGame = () =>{
+    live = true;
   alterMsgEl.innerText = "Cards: ";
 for(let x = 0;x < cardsArray.length;x++){
     alterMsgEl.textContent += cardsArray[x] + "  ";
 }
+
 sumValEl.textContent = "Sum: " + sum;
 
+sum === 21 ? gameOn = true: sum > 21 ? live = false : "";
+ 
 sum < 21 ? comment = "Draw a new card" : 
 sum === 21 ? comment = "Congratulations You've the Casino BlackJack Game" : 
 comment = "You Lost the game keep on playing";
 
-newCardMsgEl.innerText = comment;
-
+newCardMsgEl.innerText = comment;   
+sum;
 }
 
 statButton.addEventListener("click",startGame);
 
 const newCardFunc = () =>{
+    if(live === true && gameOn === false){
     let newcard = getRandomCard();
-    if(sum < 21){
     cardsArray.push(newcard);
     sum += newcard;
     startGame();
-    }
+   }
+    
 }
 
+ 
 newCardButton.addEventListener("click",newCardFunc);
 
 newGameButton.addEventListener("click",function(){
